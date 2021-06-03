df97dcff56fafed4449527eb6788b2db52b06713
pm47 <pm.padiou@gmail.com>
2021-06-03 12:07:06 +0200

    add a strategy to deal with outdated commitments
    We can either:
    - automatically request the counterparty to close the channels. This
      may be the best choice for smaller loosely administered nodes.
    - just log an error and stop the node. May be better for larger nodes,
      to prevent unwanted mass force-close.
    
    The default is to request a force-close, which was the previous
    behavior.

--- eclair-core/src/main/scala/fr/acinq/eclair/NodeParams.scala
+++ eclair-core/src/main/scala/fr/acinq/eclair/NodeParams.scala
@@ -22,6 +22,7 @@ import fr.acinq.bitcoin.{Block, ByteVector32, Crypto, Satoshi}
 import fr.acinq.eclair.Setup.Seeds
 import fr.acinq.eclair.blockchain.fee._
 import fr.acinq.eclair.channel.Channel
+import fr.acinq.eclair.channel.Channel.OutdatedCommitmentStrategy
 import fr.acinq.eclair.crypto.Noise.KeyPair
 import fr.acinq.eclair.crypto.keymanager.{ChannelKeyManager, NodeKeyManager}
 import fr.acinq.eclair.db._
@@ -76,6 +77,7 @@ case class NodeParams(nodeKeyManager: NodeKeyManager,
                       relayParams: RelayParams,
                       reserveToFundingRatio: Double,
                       maxReserveToFundingRatio: Double,
+                      outdatedCommitmentStrategy: OutdatedCommitmentStrategy,
                       db: Databases,
                       revocationTimeout: FiniteDuration,
                       autoReconnect: Boolean,
@@ -346,6 +348,11 @@ object NodeParams extends Logging {
       PathFindingExperimentConf(experiments.toMap)
     }
 
+    val outdatedCommitmentStrategy = config.getString("outdated-commitment-strategy") match {
+      case "halt-if-just-restarted" => OutdatedCommitmentStrategy.HaltIfJustRestarted
+      case "always-request-remote-close" => OutdatedCommitmentStrategy.AlwaysRequestRemoteClose
+    }
+
     val routerSyncEncodingType = config.getString("router.sync.encoding-type") match {
       case "uncompressed" => EncodingType.UNCOMPRESSED
       case "zlib" => EncodingType.COMPRESSED_ZLIB
@@ -404,6 +411,7 @@ object NodeParams extends Logging {
       ),
       reserveToFundingRatio = config.getDouble("reserve-to-funding-ratio"),
       maxReserveToFundingRatio = config.getDouble("max-reserve-to-funding-ratio"),
+      outdatedCommitmentStrategy = outdatedCommitmentStrategy,
       db = database,
       revocationTimeout = FiniteDuration(config.getDuration("revocation-timeout").getSeconds, TimeUnit.SECONDS),
       autoReconnect = config.getBoolean("auto-reconnect"),
